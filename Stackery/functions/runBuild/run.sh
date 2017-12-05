d153ec2afda7c749f9da1f84721ee96612704438
anna yovandich <anna.yovandich@ff0000.com>
2017-12-05 15:53:18 -0800

    Add run script
    Performs Git and npm commands

---
+++ Stackery/functions/runBuild/run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -x
+set -e 
+
+repo=$1
+branch=$2
+prNumber=$3
+localRepoDir=$4
+
+echo 'localRepoDir: ' $localRepoDir ', repo: ' $repo ', branch: ' $branch ', pr: ' $pr 
+
+cd $localRepoDir
+
+git 'init'
+
+git remote add origin $repo
+
+git fetch origin $branch
+
+git reset --hard FETCH_HEAD 
+
+npm install --no-progress --loglevel=error --cache '/tmp/npm' --userconfig '/tmp/npmrc'
+
+npm run build
