ea9e87b2ad71e0f74adf18ad487071531892fb1b
anna yovandich <anna.yovandich@ff0000.com>
2017-12-05 16:05:01 -0800

    Import function dependencies

--- Stackery/functions/runBuild/index.js
+++ Stackery/functions/runBuild/index.js
@@ -1,4 +1,8 @@
-const stackery = require('stackery')
+const stackery = require('stackery');
+const child_process = require('child_process');
+const path = require('path');
+const fs = require('fs-extra');
+const glob = require('glob');
 
 module.exports = function handler(message) {
   console.dir(message)
